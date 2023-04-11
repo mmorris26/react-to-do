1b3b9cf8c7463db4114d3c864e71d0901ca03aa1
Usman Bashir <me@usmanbashir.com>
2023-04-11 16:54:22 +0300

    Add validation

--- src/NewToDoForm.js
+++ src/NewToDoForm.js
@@ -3,8 +3,9 @@ import { useState } from "react"
 export default function NewToDoForm(props) {
   const [newTodo, setNewTodo] = useState('')
 
-  function handleAddTodo() {
-    // alert(newTodo);
+  function handleAddTodo(e) {
+    e.preventDefault();
+
     props.addTodo(newTodo);
     setNewTodo('');
   }
@@ -12,11 +13,16 @@ export default function NewToDoForm(props) {
   return (
     <>
       <h2>New To-Do</h2>
-      <input
-        value={newTodo}
-        onChange={(e) => setNewTodo(e.target.value)}
-        placeholder="Add a new to do task..." />
-      <button onClick={handleAddTodo}>ADD TO-DO</button>
+      <form onSubmit={handleAddTodo}>
+        <input
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Add a new to do task..."
+          required
+          pattern=".{4,}"
+        />
+        <button type="submit">ADD TO-DO</button>
+      </form>
     </>
   )
 }
